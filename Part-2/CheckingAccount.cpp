a4cbe637c309bb24a67df299e90ce4d09e15ff34
samuel-jones-uoit <60481397+samuel-jones-uoit@users.noreply.github.com>
2020-02-27 14:35:24 -0500

    Update CheckingAccount.cpp

--- Part-2/CheckingAccount.cpp
+++ Part-2/CheckingAccount.cpp
@@ -6,28 +6,37 @@ using namespace std;
 // constructor initializes balance and transaction fee
 CheckingAccount::CheckingAccount( double initialBalance, double fee ) : Account( initialBalance ) 
 {
-	// your code
+	transactionFee = fee;
 } 
 
 // credit (add) an amount to the account balance and charge fee
 void CheckingAccount::credit( double amount )
 {
-	// your code
+	Account::setBalance(Account::getBalance() + amount);
+	chargeFee();
 } 
 
 // debit (subtract) an amount from the account balance and charge fee
 bool CheckingAccount::debit( double amount )
 {
-	// your code
+	if (Account::getBalance() - transactionFee > amount){
+		Account::setBalance(Account::getBalance() - amount);
+		chargeFee();
+		return true;
+	}else{
+		return false;
+	}
 }
 
 // subtract transaction fee
 void CheckingAccount::chargeFee()
 {
-	// your code
+	Account::setBalance(Account::getBalance() - transactionFee);
 }
 
 void CheckingAccount::display(ostream & os) const
 {
-	// your code
+	os << "Account type: Checking" << endl;
+	os << "Balance: $ " << Account::getBalance() << endl;
+	os << "Transaction Fee: " << transactionFee << endl;
 }
